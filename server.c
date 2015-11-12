948ab1de99729e2433f0d200efeb1db766cea749
Zhicheng Gu <zchenggu@gmail.com>
2015-11-12 13:49:51 -0600

    11/12

--- server.c
+++ server.c
@@ -15,10 +15,12 @@
 
 pthread_mutex_t mutexT;
 pthread_cond_t empty, full;
+int head =0, tail =0, count=0;
+int numthreads, numbuffers;
 
 void getargs(int *port, int *num1, int *num2, int argc, char *argv[])
 {
-    if (argc != 2) {
+    if (argc != 4) {
 	fprintf(stderr, "Usage: %s <port>\n", argv[0]);
 	exit(1);
     }
@@ -27,40 +29,68 @@ void getargs(int *port, int *num1, int *num2, int argc, char *argv[])
     *num2 = atoi(argv[3]);
 }
 
+void *thread(void *avg){
+    int *buffer = (int *) avg;
+    int connfd;
+    while(1) {
+        pthread_mutex_lock(&mutexT);
+        while (count == 0) pthread_cond_wait(&full, &mutexT);
+
+        connfd = buffer[tail];
+        tail = (tail + 1) % numbuffers;
+        count--;
+
+        pthread_cond_signal(&empty);
+        pthread_mutex_unlock(&mutexT);
+
+        requestHandle(connfd);
+        Close(connfd);
+    }
+}
 
 int main(int argc, char *argv[])
 {
     int listenfd, connfd, port, clientlen;
     struct sockaddr_in clientaddr;
 
-    int numthreads, numbuffers;
     getargs(&port, &numthreads, &numbuffers, argc, argv);
-    int buffer =(int *) malloc(numbuffers * sizeof(int));
-    int fill=0, use=0, count=0;
+    int* buffer =(int *) malloc(numbuffers * sizeof(int));
+    pthread_mutex_init(&mutexT, NULL);
+    pthread_cond_init(&empty, NULL);
+    pthread_cond_init(&full, NULL);
 
     // 
     // CS537: Create some threads...
     //
+    pthread_t* p = (pthread_t  *) malloc(numthreads * sizeof(pthread_t));
+    int i;
+    for(i=0;i<numthreads;i++)
+        pthread_create(p+i, NULL, thread, buffer);
 
     listenfd = Open_listenfd(port);
     while (1) {
-        pthread_mutex_lock(&mutexT);
-        while (count == numbuffers) pthread_cond_wait(&empty, &mutexT);
-
         clientlen = sizeof(clientaddr);
         connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *) &clientlen);
 
+        pthread_mutex_lock(&mutexT);
+        while (count == numbuffers) pthread_cond_wait(&empty, &mutexT);
+
+        buffer[head] = connfd;
+        head = (head + 1) % numbuffers;
+        count++;
+
         pthread_cond_signal(&full);
         pthread_mutex_unlock(&mutexT);
+
         //
         // CS537: In general, don't handle the request in the main thread.
         // Save the relevant info in a buffer and have one of the worker threads
         // do the work. However, for SFF, you may have to do a little work
         // here (e.g., a stat() on the filename) ...
         //
-        requestHandle(connfd);
+        //requestHandle(connfd);
 
-        Close(connfd);
+        //Close(connfd);
     }
 
 }
